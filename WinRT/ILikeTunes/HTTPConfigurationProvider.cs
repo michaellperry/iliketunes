f8f724a849872424d3c811825d116abd6fc6f3a0
Michael L Perry <michael@qedcode.com>
2013-08-13 16:21:14 -0500

    Configure WinRT version to use the local distributor.

--- WinRT/ILikeTunes/HTTPConfigurationProvider.cs
+++ WinRT/ILikeTunes/HTTPConfigurationProvider.cs
@@ -18,7 +18,7 @@ namespace ILikeTunes
         {
             get
             {
-                string address = "https://api.facetedworlds.com/correspondence_server_web/bin";
+                string address = "http://169.254.80.80:8080/correspondence_server_web/bin";
                 string apiKey = "D1920D309A4E43EB85BD14833AFEF7E8";
 				int timeoutSeconds = 30;
                 return new HTTPConfiguration(address, "ILikeTunes", apiKey, timeoutSeconds);
